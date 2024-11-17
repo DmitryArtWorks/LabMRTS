508b522bf692c7e04f4b9ec2b2ede957cab6cc21
DmitryArtWorks <dmitryart01@gmail.com>
2024-11-17 06:22:37 +0300

    Initial commit

---
+++ template_2.py
@@ -0,0 +1,95 @@
+# %% Смоделировать гауссовский случайный процесс с заданной КФ
+"""
+В программе: 
+1) Загрузка дополнительных библиотек
+2) Определение функции фильтрации, обеспечивающей заданную КФ
+3) Инициализация параметров модели
+4) Формирование некоррелированного СП x
+5) Получение из него коррелированного СП y с заданной функцией корреляции
+6) Оценка КФ полученного СП y по его реализациям
+7) Построение графиков, содержащих 
+    - реализации процессов
+    - реализации КФ
+    - оценочная КФ, полученная усреднением множества КФ и аналитически заданная КФ.
+
+Для вывода графиков в отдельное окно используйте
+>>> %matplotlib qt
+"""
+# Загрузка дополнительных библиотек
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Фильтрация для получения образцовой КФ e^(-2*abs(x)/corr)
+def corr_filter(Z, rL_us, corr_us):
+    signal_length = Z.shape[1]
+    x = np.linspace(-rL_us/2,rL_us/2,signal_length)
+    f = np.exp(-abs(x) / (corr_us / 2))             # целевая корреляционная функция
+    F = np.sqrt(np.fft.fft(f,signal_length))        # АЧХ фильтра
+    F = F/np.max(np.abs(F))                         # нормировка АЧХ на максимальное значение: гармоники не должны училиваться
+    f = np.real(np.sqrt(2)*np.fft.ifft(np.fft.fft(Z,signal_length) * F,signal_length))
+    return f
+
+
+# %%
+# Формирование реализаций и расчёт
+# Параметры моделирования и распределения
+time_window_us = 300    # временной интервал на котором рассматривается корреляция, мкс
+f_sampling_mhz = 20     # частота дискретизации, МГц
+sigma_normal = 11       # СКО гауссовского случайного процесса
+mu_param = 0            # мат ожидание исходного процесса
+corr_int_us = 10        # интеравал корреляции, мкс
+n_samples = 20          # число усредняемых реализаций
+n_counts = int(time_window_us*f_sampling_mhz); # число отсчетов
+
+# Моделирование случайных процессов
+x = np.random.normal(mu_param, sigma_normal, (n_samples,n_counts))  # исходный СП
+y = corr_filter(x, time_window_us, corr_int_us)                     # коррелированный СП
+# Расчёт КФ
+r_est = np.zeros((n_samples, 2*n_counts-1))
+for i in range(n_samples):          # построчный расчёт корреляции
+    r_est[i,:]=np.correlate(y[i,:], y[i,:], mode='full')/ np.var(y[i,:]) / n_counts    # КФ
+ry_sample = r_est[1,:]               # КФ одной шумовой рализации
+r_est = np.mean(r_est, axis=0)      # усреднение КФ по множеству реализаций
+rx_sample = np.correlate(x[1,:], x[1,:], mode='full') / np.var(x[1,:]) / n_counts
+
+# %% 
+# Графика
+t_axis = np.linspace(0, time_window_us, n_counts)                           # ось времени реализации сигнала
+shift_axis_us = np.linspace(-time_window_us, time_window_us, 2*n_counts-1)  # ось отстройки КФ
+
+r_fun = np.exp(-abs(shift_axis_us) / (corr_int_us / 2))     # аналитически заданная КФ
+
+plt.figure()
+plt.subplot(221)                                    # Одна реализация
+plt.plot(t_axis, x[1,:], t_axis, y[1,:])
+plt.xlim(0, 10*corr_int_us)
+plt.title('Одна реализация СП')
+plt.legend(('исходный','коррелированный процесс'))
+plt.xlabel('t'), plt.ylabel('U(t)', rotation='horizontal')
+plt.subplot(222)                                    # КФ одной реализации
+plt.plot(shift_axis_us, rx_sample, shift_axis_us, ry_sample)
+plt.xlim(-3*corr_int_us, 3*corr_int_us)
+plt.title('КФ одной реализации')
+plt.legend(('исходный','коррелированный процесс'))
+plt.xlabel(r'$\tau$'), plt.ylabel(r'R($\tau$)', rotation='horizontal')
+plt.subplot(2,2,(3,4))                              # Усреднённая КФ
+plt.plot(shift_axis_us, r_est, shift_axis_us, r_fun)
+plt.xlim(-3*corr_int_us, 3*corr_int_us)
+plt.title('КФ')
+plt.legend(('моделирование','аналитически заданная'))
+plt.xlabel(r'$\tau$'), plt.ylabel(r'R($\tau$)', rotation='horizontal');
+plt.show()
+
+
+# %%
+# Выполните моделирование СП с КФ заданной по варианту
+# Проанализируйте распределение отсчётов СП до и после фильтра
+
+plt.figure()
+plt.hist([x.flatten(),y.flatten()], bins=64, density=True, label = ['исх','результ'])
+plt.legend(loc='best', frameon=False)
+plt.title('Нормальное распределение, m=' + str(mu_param) + ', sigma= ' + str(sigma_normal)) # str(sigma_param)
+plt.xlabel('y')
+plt.ylabel('W(y)', rotation='horizontal')
+plt.show()
---
+++ template_4.py
@@ -0,0 +1,86 @@
+# %% 
+# Лабораторная №4 
+"""
+В программе: 
+1) Загрузка дополнительных библиотек
+2) Генерация последовательности импульсов
+3) Вывод сигнала
+4) Вывод спектрограм
+    - с коротким окном
+    - с длинным окном и сдвигом окна на один отсчёт
+    - с длинным окном с шагом на величину окна
+5) Карты коэффициентов вейвлет-преобразования
+
+Для вывода графиков в отдельное окно используйте
+>>> %matplotlib qt
+"""
+# %%
+# Импорт и инициализация
+from matplotlib import pyplot as plt
+from toolbox import *
+
+
+# Задача - выполнить анализ сигнала с использованием спектрограмм и вейвлет-разложения
+# Исходные параметры
+N = 18
+f_d = 8e6                          # частота дискретизации, Гц
+t_d = 1/f_d                        # период дискретизации
+# t_impulse = 1/(1000 + 20*N)*1e3    # длительность импульса (t chip)
+t_impulse = 40e-6
+t_window = 80e-6                   # длительность рассматриваемого интервала
+f_carrier = (1000 + 20*N)*1e3      # частота несущей, Гц
+f_mod = 0.6e6           
+i_mod = 0.8                        # параметры модуляции (частота, глубина)
+n_chips = 8             
+# %%
+# Формирование последовательности отсчетов
+s_c = generate_sequence('chirp', t_d, n_chips, t_impulse, f_carrier);   # ЛЧМ импульс
+# s_r = generate_sequence('radio', t_d, n_chips, t_impulse, f_carrier);   # р/импульс
+# s_am = generate_sequence('AM', t_d, n_chips, t_impulse, f_carrier);     # АМ сигнал
+signal_out = s_c  # рез-татов фильтрации
+           
+# %%
+# Формирование последовательности отсчетов
+s_c = generate_sequence('chirp', t_d, n_chips, t_impulse, f_carrier);   # ЛЧМ импульс
+s_r = generate_sequence('radio', t_d, n_chips, t_impulse, f_carrier);   # р/импульс
+s_am = generate_sequence('AM', t_d, n_chips, t_impulse, f_carrier);     # АМ сигнал
+signal_out = s_c  # рез-татов фильтрации
+
+# %%
+# Синал во временной области
+plot_signal([[t_d, signal_out]])            # построение сигнала
+window_opt_len = 320
+FFT_size = 8
+plt.figure()
+
+rect = np.ones([window_opt_len, 1])
+# %%
+# Анализ спектрограмм 
+# Малый размер окна
+plt.subplot(311)
+powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(signal_out, 
+    NFFT=FFT_size,window=rect[0:FFT_size], noverlap=0)
+plt.xlabel('Sample')
+plt.ylabel('Normalized Frequency')
+# Большой размер окна с шагом на один отсчёт
+plt.subplot(312)
+powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(signal_out, 
+    NFFT=window_opt_len, window=rect, noverlap=window_opt_len-1)
+plt.xlabel('Sample')
+plt.ylabel('Normalized Frequency')
+# Большой размер окна с шагом на величину окна
+plt.subplot(313)
+powerSpectrum, freqenciesFound, time, imageAxis = plt.specgram(signal_out, 
+    NFFT=window_opt_len, window=rect, noverlap=0)
+plt.xlabel('Sample')
+plt.ylabel('Normalized Frequency')
+plt.show()
+
+
+# %%
+# Анализ карт коэффициентов вейвлет-преобразования (скейлограмм)
+cwtmatr, freqs = pywt.cwt(signal_out, np.arange(1,128), 'morl')
+plt.figure()
+plt.imshow(cwtmatr, extent=[-0, len(signal_out), 1, 128], aspect='auto',
+           vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())  
+plt.show() 
